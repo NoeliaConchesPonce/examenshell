0af63f6816dcf96428163e161ff24b554f4748fa
noelia <alumno@pc-alumno.(none)>
2020-05-04 19:30:39 +0200

    He acabado el ejercicio 3 sin errores

--- script3.sh
+++ script3.sh
@@ -1,6 +1,6 @@
 read -p "Introduce un nombre de usuario" nombreusuario
 
-usuarios=`ls -l /home | wc -l | awk '{print $4}'`
+usuarios=`ls -l /home | awk '{print $4}'`
 
 if [$nombreusuario = $usuarios]; then 
 echo "El nombre introducido corresponde a un usuario"
